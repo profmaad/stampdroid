6690aea23514f839d8b66a32516dac2eeecce575
profmaad <prof.maad@lambda-bb.de>
2013-11-18 15:29:26 +0800

    Implement settings encryption properly using RSA+AES

--- src/org/profmaad/stampdroid/AccountSettingsHelper.java
+++ src/org/profmaad/stampdroid/AccountSettingsHelper.java
@@ -9,15 +9,21 @@ import java.security.interfaces.RSAPublicKey;
 import java.security.interfaces.RSAPrivateKey;
 import javax.security.auth.x500.X500Principal;
 import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+import javax.crypto.SecretKey;
+import javax.crypto.KeyGenerator;
 
 import java.util.Calendar;
 import java.util.GregorianCalendar;
+import java.util.Map;
 
 import java.math.BigInteger;
 
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.util.Log;
+import android.util.Base64;
 import android.preference.PreferenceManager;
 import android.security.KeyPairGeneratorSpec;
 
@@ -50,35 +56,35 @@ public class AccountSettingsHelper
 
 	public String getClientID() throws Exception
 	{
-		return getValue("org.profmaad.stampdroid.client_id");
+		return getValue("org.profmaad.stampdroid.client_id", "org.profmaad.stampdroid.client_id_encyption_key", "org.profmaad.stampdroid.client_id_iv");
 	}
 
 	public String getAPIKey() throws Exception
 	{
-		return getValue("org.profmaad.stampdroid.api_key");
+		return getValue("org.profmaad.stampdroid.api_key", "org.profmaad.stampdroid.api_key_encyption_key", "org.profmaad.stampdroid.api_key_iv");
 	}
 
 	public String getAPISecret() throws Exception
 	{
-		return getValue("org.profmaad.stampdroid.api_secret");
+		return getValue("org.profmaad.stampdroid.api_secret", "org.profmaad.stampdroid.api_secret_encyption_key", "org.profmaad.stampdroid.api_secret_iv");
 	}
 
 	public void setClientID(String client_id) throws Exception
 	{
-		setValue("org.profmaad.stampdroid.client_id", client_id);
+		setValue("org.profmaad.stampdroid.client_id", "org.profmaad.stampdroid.client_id_encyption_key", "org.profmaad.stampdroid.client_id_iv", client_id);
 	}
 
 	public void setAPIKey(String api_key) throws Exception
 	{
-		setValue("org.profmaad.stampdroid.api_key", api_key);
+		setValue("org.profmaad.stampdroid.api_key", "org.profmaad.stampdroid.api_key_encyption_key", "org.profmaad.stampdroid.api_key_iv", api_key);
 	}
 
 	public void setAPISecret(String api_secret) throws Exception
 	{
-		setValue("org.profmaad.stampdroid.api_secret", api_secret);
+		setValue("org.profmaad.stampdroid.api_secret", "org.profmaad.stampdroid.api_secret_encyption_key", "org.profmaad.stampdroid.api_secret_iv", api_secret);
 	}
 
-	private String getValue(String key) throws Exception
+	private String getValue(String value_key, String encryption_key_key, String iv_key) throws Exception
 	{
 		KeyStore keystore = KeyStore.getInstance("AndroidKeyStore");
 		keystore.load(null);
@@ -92,41 +98,31 @@ public class AccountSettingsHelper
 		RSAPrivateKey private_key = (RSAPrivateKey)key_entry.getPrivateKey();
 
 		SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
-		if(!preferences.contains(key)) { return ""; }
+		if(!(preferences.contains(value_key) && preferences.contains(encryption_key_key) && preferences.contains(iv_key))) { return ""; }
+
+		byte encryption_key_encrypted[] = Base64.decode(preferences.getString(encryption_key_key, ""), Base64.DEFAULT);
+		byte iv_encrypted[] = Base64.decode(preferences.getString(iv_key, ""), Base64.DEFAULT);
 
 		Cipher rsa_cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+
 		rsa_cipher.init(Cipher.DECRYPT_MODE, private_key);
+		byte encryption_key[] = rsa_cipher.doFinal(encryption_key_encrypted);
 
-		byte value_encrypted[] = preferences.getString(key, "").getBytes("UTF-8");
-		byte value_decrypted_raw[] = new byte[rsa_cipher.getOutputSize(value_encrypted.length)];
+		rsa_cipher.init(Cipher.DECRYPT_MODE, private_key);
+		byte iv[] = rsa_cipher.doFinal(iv_encrypted);
 
-		Log.i(log_tag, "ENCRYPTED SIZE: "+value_encrypted.length);
-		Log.i(log_tag, "DECRYPTED SIZE: "+value_decrypted_raw.length);
-
-		int offset = 0;
-		int output_offset = 0;
-		while(offset < value_encrypted.length)
-		{
-			int block_size = rsa_cipher.getBlockSize();
-			if(offset + block_size > value_encrypted.length)
-			{
-				block_size = value_encrypted.length - offset;
-			}
-
-			output_offset = rsa_cipher.update(value_encrypted, offset, block_size, value_decrypted_raw, output_offset);
-			Log.i(log_tag, String.format("Offset: %d, block size: %d, output offset: %d", offset, block_size, output_offset));
-
-			offset += block_size;
-		}
-		Log.i(log_tag, String.format("output offset: %d", output_offset));
-		rsa_cipher.doFinal(value_decrypted_raw, output_offset);
+		Cipher aes_cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+		aes_cipher.init(Cipher.DECRYPT_MODE, new SecretKeySpec(encryption_key, "AES"), new IvParameterSpec(iv));		
+		
+		byte value_encrypted[] = Base64.decode(preferences.getString(value_key, ""), Base64.DEFAULT);
+		byte value_decrypted_raw[] = aes_cipher.doFinal(value_encrypted);
 
 		String value = new String(value_decrypted_raw, "UTF-8");
 
 		return value;
 	}
 
-	private void setValue(String key, String value) throws Exception
+	private void setValue(String value_key, String encryption_key_key, String iv_key, String value) throws Exception
 	{
 		KeyStore keystore = KeyStore.getInstance("AndroidKeyStore");
 		keystore.load(null);
@@ -138,48 +134,37 @@ public class AccountSettingsHelper
 
 		KeyStore.PrivateKeyEntry key_entry = (KeyStore.PrivateKeyEntry)keystore.getEntry(KEYSTORE_KEY_ALIAS, null);
 		RSAPublicKey public_key = (RSAPublicKey)key_entry.getCertificate().getPublicKey();
-		Log.i(log_tag, "Public key: "+public_key.toString());
+
+		KeyGenerator aes_key_generator = KeyGenerator.getInstance("AES");
+		aes_key_generator.init(256);
+		SecretKey encryption_key = aes_key_generator.generateKey();
+
+		Cipher aes_cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+		aes_cipher.init(Cipher.ENCRYPT_MODE, encryption_key);
+
+		byte value_decrypted[] = value.getBytes("UTF-8");
+		byte value_encrypted_raw[] = aes_cipher.doFinal(value_decrypted);
+		String value_encrypted = Base64.encodeToString(value_encrypted_raw, Base64.DEFAULT);
+
+		byte iv[] = aes_cipher.getIV();
 
 		Cipher rsa_cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
 
-		Log.i(log_tag, "Value: "+value);
-		byte value_decrypted[] = value.getBytes("UTF-8");
-		
 		rsa_cipher.init(Cipher.ENCRYPT_MODE, public_key);
+		byte iv_encrypted_raw[] = rsa_cipher.doFinal(iv);
+		String iv_encrypted = Base64.encodeToString(iv_encrypted_raw, Base64.DEFAULT);
 
-		byte value_encrypted_raw[] = new byte[rsa_cipher.getOutputSize(value_decrypted.length)];
-
-		Log.i(log_tag, "DECRYPTED SIZE: "+value_decrypted.length);
-		Log.i(log_tag, "ENCRYPTED SIZE: "+value_encrypted_raw.length);
-
-		int offset = 0;
-		int output_offset = 0;
-		while(offset < value_decrypted.length)
-		{
-			int block_size = rsa_cipher.getBlockSize();
-			if(offset + block_size > value_decrypted.length)
-			{
-				block_size = value_decrypted.length - offset;
-			}
-
-			output_offset += rsa_cipher.update(value_decrypted, offset, block_size, value_encrypted_raw, output_offset);
-			Log.i(log_tag, String.format("Offset: %d, block size: %d, output offset: %d", offset, block_size, output_offset));
-
-			offset += block_size;
-		}
-		Log.i(log_tag, String.format("output offset: %d", output_offset));
-		rsa_cipher.doFinal(value_encrypted_raw, output_offset);
-		
-		String value_encrypted = new String(value_encrypted_raw, "UTF-8");
-		Log.i(log_tag, "Value encrypted: "+value_encrypted.length());
+		rsa_cipher.init(Cipher.ENCRYPT_MODE, public_key);
+		byte encryption_key_encrypted_raw[] = rsa_cipher.doFinal(encryption_key.getEncoded());
+		String encryption_key_encrypted = Base64.encodeToString(encryption_key_encrypted_raw, Base64.DEFAULT);
 
 		SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
 
 		preferences.edit()
-			.putString(key, value_encrypted)
+			.putString(value_key, value_encrypted)
+			.putString(encryption_key_key, encryption_key_encrypted)
+			.putString(iv_key, iv_encrypted)
 			.commit();
-
-		Log.i(log_tag, "Value from prefs: "+preferences.getString(key, "").length());
 	}
 
 	private boolean isKeyPresent() throws Exception
@@ -208,5 +193,16 @@ public class AccountSettingsHelper
 		keypair_generator.initialize(keypair_generator_spec);
 
 		KeyPair keypair = keypair_generator.generateKeyPair();
+	}
+
+	private void dumpPreferences()
+	{
+		SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
+		Map<String,?> keys = preferences.getAll();
+		
+		for(Map.Entry<String,?> entry : keys.entrySet()){
+            Log.i("PREF VALUES",entry.getKey() + ": " + 
+				  entry.getValue().toString());            
+		}
 	}
 }
